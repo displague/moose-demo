ab6b412ee5b0f076887789bce5a63f3908d315f0
Marques Johansson <marques@displague.com>
2014-11-17 23:50:58 -0500

    Validation failed for 'ArrayRef[Planet::Earth::Country]' with value 1

--- Planet/Earth/Country.pm
+++ Planet/Earth/Country.pm
@@ -1,17 +1,21 @@
 class Planet::Earth::Country {
   has name => (is=>'rw', isa=>'Str',required=>1);
 
+  my %countries = (
+    'North America' => ['United States','Canada','Mexico'],
+    Europe => ['France', 'Germany', 'Italy'],
+    'South America' => ['Brazil', 'Paraguay', 'Uruguay'],
+    # 'Antarctica' intentionally empty.  Handle undefs
+    Africa => ['Egypt', 'South Africa', 'Sudan'],
+    Australia => ['Australia','New Zealand'],
+    Asia => ['China','Japan','India']
+  );
+
   sub getCountries {
     my $continent = shift;
-    my %countries = (
-    	'North America' => ['United States','Canada','Mexico'],
-	Europe => ['France', 'Germany', 'Italy'],
-	'South America' => ['Brazil', 'Paraguay', 'Uruguay'],
-	# 'Antartica' intentionally empty.  Handle undefs
-	Africa => ['Egypt', 'South Africa', 'Sudan'],
-	Australia => ['Australia','New Zealand'],
-	Asia => ['China','Japan','India']
-    );
-    %countries->{$continent};
+    use Data::Dumper;
+    map { print $continent; print Dumper($_); Planet::Earth::Country->new(name=>$_) if $_ } %countries->{$continent};
   }
+
 }
+
